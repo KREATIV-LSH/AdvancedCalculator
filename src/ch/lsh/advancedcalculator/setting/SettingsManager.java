fd9d36d25fa56363516d945f8b364a2b20c351ee
KRTEATIV-LSH <lshutterli@gmail.com>
2021-02-22 20:34:13 +0100

    SettingsManager

--- src/ch/lsh/advancedcalculator/setting/SettingsManager.java
+++ src/ch/lsh/advancedcalculator/setting/SettingsManager.java
@@ -1,5 +1,11 @@
 package ch.lsh.advancedcalculator.setting;
 
 public class SettingsManager {
+	
+	private SettingsManager instance;
+	
+	public SettingsManager getInstance() {
+		return instance;
+	}
 
 }
